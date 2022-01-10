1c124d00e4d35d3cab6364cbcc6320fb34c55fe2
Orangera1n <96156354+Orangera1n@users.noreply.github.com>
2022-01-10 16:18:49 -0500

    Added more things.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-print("Welcome to TestApplication version 0.1 (Build 2.fbl_Testapplication(orangera1n).211214-1958")
+print("Welcome to TestApplication version 0.1 (Build 3.fbl_Testapplication(orangera1n).220111-1619
 x=int(input("How many days since an incident:"))
 if x<0:
   print("Sorry, that is not a number, but it is a poop emoji")
-elif x<10:
+elif x<3:
+      print("Sorry, but you really should improve your safety")
+elif X>3 and x<10:
   print("Your safety is not great")
 elif x>10 and x<30:
     print("Your safety is not too bad")
